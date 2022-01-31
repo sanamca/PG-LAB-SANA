c020846d94e4e65d8559ff09c2dbf9b20975f5a4
sanamca <95209065+sanamca@users.noreply.github.com>
2022-01-31 16:29:21 +0530

    Delete co2 programs directory

--- co2 programs/co2_1.py
+++
@@ -1,5 +0,0 @@
-n=int(input("enter the element"))
-f=1
-for i in range(1,n+1):
-    f=f*i
-print("factorial of",n,"=",f)
--- co2 programs/co2_7.py
+++
@@ -1,7 +0,0 @@
-str=(input("enter the string"))
-print("input string:",str)
-if(str.endswith("ing")):
-    str=str='ly'
-else:
-    str=str+'ing'
-    print("the result string is",str)
