fde2e1d87460232b133282510bf7c5cd3deb9ac1
sanamca <95209065+sanamca@users.noreply.github.com>
2022-02-11 14:26:38 +0530

    Delete co1_2.2.py

--- co1_2.2.py
+++
@@ -1,8 +0,0 @@
-s=int(input("enter the 1st year"))
-l=int(input("enter the last year"))
-if(s<l):
-    print("laep yera is",end=" ")
-for i in range(s,l):
-    if i%4==0 and i%100!=0:
-        print(i,end=" ")
-        
